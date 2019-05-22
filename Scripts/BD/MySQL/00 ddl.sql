14137b2b115d4929f11d2c939f4194f58e5473ef
Caro24baez <caro24baez@gmail.com>
2019-05-22 16:18:35 -0300

    Realización del ddl

--- Scripts/BD/MySQL/00 ddl.sql
+++ Scripts/BD/MySQL/00 ddl.sql
@@ -1,6 +1,77 @@
-CREATE SCHEMA cine;
-USE cine;
+CREATE TABLE PeliculaGenero
+(
+	idPelicula SMALLINT NOT NULL,
+	idGenero TINYINT NOT NULL,
+	KEY (idGenero)
+) 
+;
 
-CREATE TABLE Pelicula( idPelicula SMALLINT,
-					   nombre VARCHAR(45),
-                       fechaLanzamiento DATETIME
+
+CREATE TABLE Genero
+(
+	idGenero TINYINT NOT NULL,
+	genero TINYINT NOT NULL,
+	PRIMARY KEY (idGenero)
+) 
+;
+
+
+CREATE TABLE Entrada
+(
+	idEntrada INTEGER NOT NULL,
+	numeroEntrada INTEGER NOT NULL,
+	funcion VARCHAR(45) NOT NULL,
+	valor INTEGER NOT NULL,
+	PRIMARY KEY (idEntrada)
+) 
+;
+
+
+CREATE TABLE Sala
+(
+	idSala TINYINT NOT NULL,
+	piso TINYINT NOT NULL,
+	capacidad TINYINT NOT NULL,
+	PRIMARY KEY (idSala)
+) 
+;
+
+
+CREATE TABLE Proyeccion
+(
+	idProyeccion INTEGER NOT NULL,
+	idPelicula SMALLINT NOT NULL,
+	fechaHora DATETIME NOT NULL,
+	idSala INTEGER NOT NULL,
+	idEntrada INTEGER NOT NULL,
+	PRIMARY KEY (idProyeccion),
+	KEY (idEntrada),
+	KEY (idPelicula)
+) 
+;
+
+
+CREATE TABLE Pelicula
+(
+	idPelicula SMALLINT NOT NULL,
+	nombre VARCHAR(45) NOT NULL,
+	fechaLanzamiento DATETIME NOT NULL,
+	PRIMARY KEY (idPelicula)
+) 
+;
+
+
+
+
+
+ALTER TABLE PeliculaGenero ADD CONSTRAINT FK_PeliculaGenero_Genero 
+	FOREIGN KEY (idGenero) REFERENCES Genero (idGenero)
+;
+
+ALTER TABLE Proyeccion ADD CONSTRAINT FK_Proyeccion_Entrada 
+	FOREIGN KEY (idEntrada) REFERENCES Entrada (idEntrada)
+;
+
+ALTER TABLE Proyeccion ADD CONSTRAINT FK_Proyeccion_Pelicula 
+	FOREIGN KEY (idPelicula) REFERENCES Pelicula (idPelicula)
+;
