f12143910ac585740a649f0e82c1740c9b97370e
RaymiIrahola <raymiirahola@gmail.com>
2019-05-22 17:09:44 -0300

    Iniciacion de SF con una correcion del Diagrama de clases

---
+++ Scripts/BD/MySQL/01 sf.sql
@@ -0,0 +1,18 @@
+DELIMITER $$
+CREATE FUNCTION entradasVendidasEn(unIdPelicula SMALLINT) RETURNS FLOAT entrada
+
+BEGIN 
+	declare entrada float;
+
+
+END $$
+
+
+DELIMITER $$
+CREATE FUNCTION entradasDisponibles(unIdProyeccion SMALLINT)
+
+BEGIN 
+
+
+
+END$$ 
