f207c4a1ffb7de05c6021e068d22ca7239c051e4
RaymiIrahola <raymiirahola@gmail.com>
2019-05-28 15:12:31 -0300

    Finalización del Store Function

--- Scripts/BD/MySQL/01 sf.sql
+++ Scripts/BD/MySQL/01 sf.sql
@@ -1,21 +1,37 @@
-DELIMITER $$
-CREATE FUNCTION entradasVendidasEn(unIdPelicula SMALLINT, fechaInferior DATETIME, fechaSuperior DATETIME) RETURNS INT
+DELIMITER $$ 
+CREATE FUNCTION entradasVendidasEntre (unidPelicula SMALLINT, fechaInferior DATETIME, fechaSuperior DATETIME) 
+									RETURNS INT
 
 BEGIN 
-	declare entrada INT;
-    select count(idEntrada) into entrada
-    from Entrada E
-    where 
-
-	return entrada;
+	DECLARE entrada INT;
+    
+    SELECT count(idEntrada) INTO entrada
+    FROM Proyeccion  
+    WHERE idPelicula = unidPelicula
+    AND fechaHora BETWEEN fechaInferior AND fechSuperior;
+    
+    
+	RETURN entrada;
 END $$
+
+
 
 
 DELIMITER $$
 CREATE FUNCTION entradasDisponibles(unIdProyeccion SMALLINT)
+								RETURNS INT
 
 BEGIN 
+	DECLARE entradaDisponible INT;
+    
+    SELECT (capacidadSala - cantidadEntradasVendidas) INTO EntradaDisponible
+    FROM Proyeccion P
+    INNER JOIN Entrada E
+		ON P.idEntrada = E.idEntrada
+	INNER JOIN Sala S
+		ON S.idSala = P.idSala
+	WHERE idProyeccion = unidProyeccion;
+    
+    RETURN entradaDisponible;
 
-
-
-END$$ 
+END $$ 
