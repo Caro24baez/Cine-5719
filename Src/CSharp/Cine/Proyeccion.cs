6a9d03eeb460f4aed25ec48f87cc2c429b187a5d
RaymiIrahola <raymiirahola@gmail.com>
2019-08-21 17:47:08 -0300

    Agregación de atributo y clases
    Agregar atributos en la clase proyeccion y sala. Y creacion de clases del diagrama de clase "Proyeccion" y "Sala"

--- Src/CSharp/Cine/Proyeccion.cs
+++ Src/CSharp/Cine/Proyeccion.cs
@@ -1,10 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace Cine
 {
-    class Proyeccion
+    [Table("Proyeccion")]
+    public class Proyeccion
     {
+       
+        [Key, Column("idProyeccion")]
+        public short IdProyeccion { get; set; }
+        [ForeignKey("idPelicula"), Required]
+        public Pelicula Pelicula { get; set; }
+        [Column("fechaHora")]
+        public DateTime FechaHora { get; set; }
+        [Column("idSala")]
+        public byte IdSala { get; set; }
+        [ForeignKey("idEntrada"), Required]
+        public Entrada Entrada { get; set; }
+
+        public Proyeccion() { }
+        public Proyeccion(Pelicula pelicula, Entrada entrada)
+        {
+            Pelicula = pelicula;
+            Entrada = entrada;
+            FechaHora = DateTime.Now;
+        }
+
+        public bool HoraFuncion(DateTime inicio, DateTime fin)
+        {
+            return inicio <= FechaHora && FechaHora <= fin;
+        }
     }
 }
