0a615e7092958db78254ea7e26fabe09cf66bc10
RaymiIrahola <raymiirahola@gmail.com>
2019-08-28 16:13:12 -0300

    Agregar Lista
    Cada clase tiene agregado una lista en el IADO

--- Src/CSharp/Cine/Ado/IADO.cs
+++ Src/CSharp/Cine/Ado/IADO.cs
@@ -6,6 +6,32 @@ namespace Cine.Ado
 {
     public interface IADO
     {
-     #
+        #region Pelicula
+        void agregarPelicula(Pelicula pelicula);
+        List<Pelicula> obtenerPelicula();
+
+        #endregion
+
+        #region Genero
+        void agregarGenero(Genero genero);
+        List<Genero> obtenerGenero();
+        #endregion
+
+        #region Entrada
+        void agregarEntrada(Entrada entrada);
+        void actualizarEntrada(Entrada entrada);
+        List<Entrada> obtenerEntrada();
+        List<Proyeccion> proyeccionDe(Entrada entrada);
+        #endregion
+
+        #region Sala
+        void agregarSala(Sala sala);
+        List<Sala> obtenerSala();
+        #endregion
+
+        #region Proyeccion
+        void agregarProyeccion(Proyeccion proyeccion);
+        List<Proyeccion> obtenerProyeccion();
+        #endregion
     }
 }
