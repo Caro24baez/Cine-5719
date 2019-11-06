a0532af27240179c1b409dda454edd8004978de4
RaymiIrahola <raymiirahola@gmail.com>
2019-11-06 16:24:58 -0300

    Creacion de la clase cajero y sus atributos

---
+++ Src/CSharp/Cine/Cajero.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace Cine
+{
+
+    [Table("Cajero")]
+    public class Cajero
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key, Column("idCajero")]
+        public short IdCajero { get; set; }
+
+        [ForeignKey("idEntrada"), Required]
+        public Entrada entrada { get; set; }
+
+        [Column("nombre"), StringLength(45), Required]
+        public string nombre { get; set; }
+
+        [Column("apellido"), StringLength(45), Required]
+        public string apellido { get; set; }
+
+        [Column("contraseña"), StringLength(45), Required]
+        public string contraseña { get; set; }
+
+        [Column("email"), StringLength(45), Required]
+        public string email { get; set; }
+
+
+    }
+}
