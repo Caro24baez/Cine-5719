d0607b22d7195fd81616cacc347d672249ccf1a6
Caro24baez <caro24baez@gmail.com>
2019-11-06 16:38:44 -0300

    arreglos por haber comiteado a la vez

--- Src/CSharp/Cine/Cajero.cs
+++ Src/CSharp/Cine/Cajero.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-<<<<<<< HEAD
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -23,37 +22,7 @@ namespace Cine
         public string Email { get; set; }
         [Column("contraseña"), StringLength(45), Required]
         public string Contraseña { get; set; }
-=======
-using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
 
-namespace Cine
-{
-
-    [Table("Cajero")]
-    public class Cajero
-    {
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [Key, Column("idCajero")]
-        public short IdCajero { get; set; }
-
-        [ForeignKey("idEntrada"), Required]
-        public Entrada entrada { get; set; }
-
-        [Column("nombre"), StringLength(45), Required]
-        public string nombre { get; set; }
-
-        [Column("apellido"), StringLength(45), Required]
-        public string apellido { get; set; }
-
-        [Column("contraseña"), StringLength(45), Required]
-        public string contrasenia { get; set; }
-
-        [Column("email"), StringLength(45), Required]
-        public string email { get; set; }
-
->>>>>>> 1444c6b51d182105f40d4ba5baa84e68d5440b8c
 
     }
 }
