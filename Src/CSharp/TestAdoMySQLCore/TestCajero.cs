694ac8b08bfd55cab7512dda6a76341ef0828ef9
Caro24baez <caro24baez@gmail.com>
2019-11-07 15:02:09 -0300

    test cajero

---
+++ Src/CSharp/TestAdoMySQLCore/TestCajero.cs
@@ -0,0 +1,21 @@
+﻿using Cine.Ado;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestAdoMySQLCore
+{
+    [TestClass]
+    public class TestCajero
+    {
+        private IADO Ado { get; set; }
+
+        [ClassInitialize]
+        public static void fixture(TestContext context)
+        {
+           
+           
+        }
+    }
+}
