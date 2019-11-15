44c688e10ab1d01883bd58bf2fa531b252d8a6c4
RaymiIrahola <raymiirahola@gmail.com>
2019-11-15 17:53:22 -0300

    Se agrego como referencia y se heredera el menu componente

--- Src/CSharp/ConsolaCajero/Cajero/AltaCajero.cs
+++ Src/CSharp/ConsolaCajero/Cajero/AltaCajero.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using MenuesConsola;
 
 namespace ConsolaCajero.Cajero
 {
-    public class AltaCajero
+    public class AltaCajero : MenuComponente
     {
         public Cajero Cajero{ get; set; }
 
