928555368595ba8155e8aafa6b9fcff265a14e09
RaymiIrahola <raymiirahola@gmail.com>
2019-11-22 17:34:25 -0300

    Se agrego el menu de pelicula

--- Src/CSharp/ConsolaAdministrador/Menu/MenuAgregarPelicula.cs
+++ Src/CSharp/ConsolaAdministrador/Menu/MenuAgregarPelicula.cs
@@ -2,11 +2,27 @@
 using System.Collections.Generic;
 using System.Text;
 using Cine;
+using MenuesConsola;
 
 namespace ConsolaAdministrador.Menu
 {
-    public class MenuAgregarPelicula
+    public class MenuAgregarPelicula : MenuComponente
     {
+        public Pelicula Pelicula { get; set; }
 
+        public MenuListaPelicula MenuListaPelicula { get; set; }
+
+        public MenuAgregarPelicula(MenuListaPelicula menuListaPelicula)
+        {
+            MenuListaPelicula = menuListaPelicula;
+            Nombre = "Agregar pelicula";
+
+        }
+
+        public override void mostrar()
+        {
+            base.mostrar();
+            var nombre = prompt("Ingrese el nombre de la pelicula");
+        }
     }
 }
