f13927ea378bea7a5e4af9d4b9eadc25d24fbf15
Caro24baez <caro24baez@gmail.com>
2019-11-28 15:12:01 -0300

    ...

--- Src/CSharp/ConsolaAdministrador/Menu/MenuAgregarSala.cs
+++ Src/CSharp/ConsolaAdministrador/Menu/MenuAgregarSala.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using MenuesConsola;
+using Cine;
 namespace ConsolaAdministrador.Menu
 {
-    public class MenuAgregarSala
+    public class MenuAgregarSala : MenuComponente
     {
+        public Sala Sala { get; set; }
+
+        //public MenuAgregarSala MenuAgregarSala { get; set; }
 
     }
 }
